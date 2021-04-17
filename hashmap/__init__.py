59689341dd296cc71994639adccfc95d840b45bd
Kenny Baskett <kbaskett248@gmail.com>
2021-04-17 17:49:52 -0400

    Prefix attributes to indicate they should not be accessed

--- hashmap/__init__.py
+++ hashmap/__init__.py
@@ -12,16 +12,16 @@ class Pair(typing.NamedTuple):
 
 
 class HashMap(collections.abc.MutableMapping):
-    size_factor: int
-    num_buckets: int
-    length: int
-    buckets: typing.List[typing.Optional[Pair]]
+    _size_factor: int
+    _num_buckets: int
+    _length: int
+    _buckets: typing.List[typing.Optional[Pair]]
 
     def __init__(self, *args, **kwargs):
-        self.size_factor = 9
-        self.buckets = []
+        self._size_factor = 9
+        self._buckets = []
         self._expand()
-        self.length = 0
+        self._length = 0
 
         for iterable in args:
             for key, value in iterable:
@@ -39,15 +39,15 @@ class HashMap(collections.abc.MutableMapping):
         Returns:
             int: The index in buckets for the given key
         """
-        return hash(key) % self.num_buckets
+        return hash(key) % self._num_buckets
 
     def _expand(self):
         """Double the number of buckets available in the hashmap"""
-        old_pairs = self.buckets
-        self.size_factor += 1
-        self.num_buckets = 2 ** self.size_factor - 1
-        self.buckets = [None for _ in range(self.num_buckets)]
-        self.length = 0
+        old_pairs = self._buckets
+        self._size_factor += 1
+        self._num_buckets = 2 ** self._size_factor - 1
+        self._buckets = [None for _ in range(self._num_buckets)]
+        self._length = 0
 
         for pair in old_pairs:
             if pair is None:
@@ -63,13 +63,13 @@ class HashMap(collections.abc.MutableMapping):
         """
         while True:
             index = self._get_index(key)
-            pair = self.buckets[index]
+            pair = self._buckets[index]
             if pair is None:
-                self.buckets[index] = Pair(key, value)
-                self.length += 1
+                self._buckets[index] = Pair(key, value)
+                self._length += 1
                 break
             elif pair.key == key:
-                self.buckets[index] = Pair(key, value)
+                self._buckets[index] = Pair(key, value)
                 break
             else:
                 self._expand()
@@ -88,7 +88,7 @@ class HashMap(collections.abc.MutableMapping):
             typing.Any: The value from the HashMap for the given key, or the
                 default value if it doesn't exist
         """
-        pair = self.buckets[self._get_index(key)]
+        pair = self._buckets[self._get_index(key)]
         if (pair is None) or (pair.key != key):
             if default == _SENTINEL_VALUE:
                 raise KeyError(f"key does not exist: {key}")
@@ -110,12 +110,12 @@ class HashMap(collections.abc.MutableMapping):
         Returns:
             typing.Any: The value associated with the given key.
         """
-        pair = self.buckets[self._get_index(key)]
+        pair = self._buckets[self._get_index(key)]
         if (pair is None) or (pair.key != key):
             raise KeyError(f"key does not exist: {key}")
         else:
-            self.length -= 1
-            self.buckets[self._get_index(key)] = None
+            self._length -= 1
+            self._buckets[self._get_index(key)] = None
             return pair.value
 
     def __setitem__(self, key: typing.Hashable, value: typing.Any) -> None:
@@ -128,7 +128,7 @@ class HashMap(collections.abc.MutableMapping):
         self.delete(key)
 
     def __iter__(self) -> typing.Iterator[typing.Tuple[typing.Hashable, typing.Any]]:
-        return (pair for pair in self.buckets if pair is not None)
+        return (pair for pair in self._buckets if pair is not None)
 
     def __len__(self) -> int:
-        return self.length
+        return self._length
